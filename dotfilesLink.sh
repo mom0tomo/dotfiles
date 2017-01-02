b895436bc9f5a779dba647c202c46e3103a6bef4
momo <mo-watanabe@dip-net.co.jp>
2017-01-03 00:24:27 +0900

    シンボリックリンクを貼るファイル群を修正

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/colors ~/.vim
-ln -sf ~/dotfiles/ftdetect ~/.vim
-ln -sf ~/dotfiles/indent ~/.vim
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/.wgetrc ~/.wgetrc
-ln -sf ~/dotfiles/Brewfile ~/Brewfile
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
