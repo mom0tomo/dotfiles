9ebf0dc06c6fea27d63fc18b390d9ec2aa7ffe78
mo-watanabe <mo-watanabe@shisutemukaihatsubukanrisha-no-MacBook-Pro.local>
2017-03-29 10:50:32 +0900

    ファイル構成とセットアップ方法を変更

--- dotfilesLink.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-for f in .??*
-do
-    [[ "$f" == ".git" ]] && continue
-    [[ "$f" == ".DS_Store" ]] && continue
-
-    echo "$f"
-done
---
+++ setup.sh
@@ -0,0 +1,5 @@
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.zshenv ~/.zshenv
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.gitignore ~/.gitignore
