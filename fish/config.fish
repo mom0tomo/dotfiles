9d2f3556fc3251576a6958c5657419352753cd9c
momo <mo-watanabe@dip-net.co.jp>
2017-07-04 13:32:02 +0900

    devide functions from config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -7,22 +7,8 @@ set -x PATH /usr/local/bin /usr/local/sbin $PATH
 # homebrew
 set -U fish_user_paths /usr/local/bin $fish_user_paths
 
-# use gvm
-function gvm
-  bass source ~/.gvm/scripts/gvm ';' gvm $argv
-end
-
 # plugins
 set fish_plugin peco
-
-# peco
-function peco
-  command peco --layout=bottom-up $argv
-end
-
-function fish_user_key_bindings
-  bind \cr 'peco_select_history (commandline -b)'
-end
 
 # alias
 alias vi 'vim'
@@ -39,12 +25,6 @@ alias sudo 'sudo -E env PATH=$PATH'
 alias sudo 'sudo'
 
 alias relogin 'source ~/.config/fish/config.fish'
-
-# cd > ls -la
-function cd
-  builtin cd $argv
-  ls -la
-end
 
 # vi mode
 fish_vi_key_bindings
