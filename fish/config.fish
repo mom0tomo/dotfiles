ad05fbfed56887a1eeead4e623bec0dd00a9603f
momo <momo76w@gmail.com>
2017-09-02 15:43:45 +0900

    remove dangerous paths

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,8 @@
 # encoding
 set -x LANG ja_JP.UTF-8
 
-# PATH
-set -x PATH /usr/local/bin /usr/local/sbin $PATH
-
-# homebrew
-set -U fish_user_paths /usr/local/bin $fish_user_paths
-
 # go
-set -U fish_user_paths $GOPATH/bin $fish_user_paths
+# set PATH $PATH $GOPATH/bin
 
 # plugins
 set fish_plugin peco
@@ -24,10 +18,4 @@ alias mv 'mv -i'
 
 alias mkdir 'mkdir -p'
 
-alias sudo 'sudo -E env PATH=$PATH'
-alias sudo 'sudo'
-
 alias relogin 'source ~/.config/fish/config.fish'
-
-# vi mode
-fish_vi_key_bindings
