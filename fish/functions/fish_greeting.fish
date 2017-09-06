881deaea0f74937c2eb52297b0b3c3ed09ff2a30
momo <mo-watanabe@dip-net.co.jp>
2017-09-06 15:15:38 +0900

    change greetings

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,4 +1,4 @@
 # Defined in /Users/momo/.config/fish/functions/fish_greeting.fish @ line 1
 function fish_greeting
-	echo "人間は負けたら終わりなのではない。辞めたら終わりなのだ。"
+	echo "プログラミング言語は世界のほんのちっぽけな一部分にすぎない。その程度のものだから、あまり真剣に取り過ぎるべきではない。"
 end
