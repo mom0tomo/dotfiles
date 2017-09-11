431769fb4109c9030f159a90f08f6e17bc2f93b7
momo <mo-watanabe@dip-net.co.jp>
2017-09-11 12:57:49 +0900

    change the message

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,4 +1,4 @@
 # Defined in /Users/momo/.config/fish/functions/fish_greeting.fish @ line 1
 function fish_greeting
-	echo "プログラミング言語は世界のほんのちっぽけな一部分にすぎない。その程度のものだから、あまり真剣に取り過ぎるべきではない。"
+	echo "This thing that we call 'failure' is not the falling down, but the staying down."
 end
